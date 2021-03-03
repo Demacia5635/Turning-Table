42efe59b8d590579b0a833b95a59c47c0218e3f6
Yaron Drukh <47784310+Yarondr@users.noreply.github.com>
2021-03-03 22:05:32 +0200

    Add files via upload

---
+++ turningtable.py
@@ -0,0 +1,17 @@
+import csv 
+
+l1 =[]
+l2 = []
+l3 = []
+l4 = []
+
+#go throught csv mapping speed to collum. round up each element 
+
+table = {}
+
+
+
+for i in l1:
+	for j in range(len(l2)):
+		table[l2[j]] = l3[j] + ":" + l4[j]
+		table[i] = table[l2[j]]	
